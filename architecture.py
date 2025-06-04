1fcc863f7fb56f7dc143f86ed903c10d342f0d39
Richard Berenczei <104381076+berenczeiricsi@users.noreply.github.com>
2025-06-04 09:48:24 +0200

    Fix conv block output channels

--- architecture.py
+++ architecture.py
@@ -6,7 +6,8 @@ class CNN(nn.Module):
     def __init__(self, input_shape: int, hidden_units: int, output_shape: int):
         super().__init__()
         self.conv_block_1 = nn.Sequential(
-            nn.Conv2d(in_channels=input_shape, out_channels=output_shape, kernel_size=3, stride=1, padding=1),
+            # First convolution increases channel depth to the hidden unit size
+            nn.Conv2d(in_channels=input_shape, out_channels=hidden_units, kernel_size=3, stride=1, padding=1),
             nn.BatchNorm2d(hidden_units),
             nn.ReLU(),
             nn.Conv2d(in_channels=hidden_units, out_channels=hidden_units, kernel_size=3, stride=1, padding=1),
